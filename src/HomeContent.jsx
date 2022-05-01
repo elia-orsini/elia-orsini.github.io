054340af06ed5b6ecfbde37d6ff1b79a28c106ba
Your name here <elia.orsini@hotmail.com>
2022-05-01 10:41:18 +0100

    initialise

---
+++ src/HomeContent.jsx
@@ -0,0 +1,34 @@
+
+import React, { useRef, useEffect } from 'react'
+import R3f from './R3f'
+import Typewriter from 'typewriter-effect';
+
+export default function HomeContent({ ...props }) {
+  return (
+    <>
+      <div className="absolute text-[4.5rem] mr-[36rem] mt-[27rem] z-10 font-inter font-bold z-10">
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter.typeString("Hiya, I'm El")
+            .pauseFor(200)
+            .typeString("ig")
+            .pauseFor(400)
+            .deleteChars(1)
+            .typeString("a")
+            .pauseFor(700)
+            .typeString(".")
+            .start()
+          }}
+          options={{
+            autoStart: true,
+            deleteSpeed: 0.5,
+            delay: 200
+          }}
+          />
+      </div>
+      {/* <div className='opacity-100 absolute top-[20rem] w-[50rem] h-[18rem] bg-slate-700 bg-gradient-to-t from-emerald-400'>
+      </div> */}
+      <R3f  className="z-0"/>
+    </>
+  )
+}
---
+++ src/Photography.jsx
@@ -0,0 +1,154 @@
+
+import React, { useRef, useEffect } from 'react'
+
+export default function Photography(props) {
+
+  function renderPhotos() {
+    switch(props.photosIndex) {
+      case 0:
+        return (
+          <>
+            {/* <span className='absolute mt-20 ml-2 z-0'>light shines in the darkeness of the night;</span> */}
+
+            <div className='mx-auto my-auto flex flex-col gap-1 w-full h-full mt-40 pt-2 px-2 bg-black z-10'>
+              <div className='relative h-[100rem]'>
+                <img src="/photography/nightlife/1.jpg" layout="fill" objectFit="contain"/>
+              </div>
+              <div className='relative h-[100rem]'>
+                <img src="/photography/nightlife/2.jpg" layout="fill" objectFit="contain"/>
+              </div>
+              <div className='relative h-[100rem]'>
+                <img src="/photography/nightlife/3.jpg" layout="fill" objectFit="contain"/>
+              </div>
+            </div>
+
+            <div className='mx-auto my-auto flex flex-col gap-1 w-full h-full mt-40 pt-2 px-2 bg-black z-10'>
+              <div className='relative h-[100rem]'>
+                <img src="/photography/nightlife/4.jpg" layout="fill" objectFit="contain"/>
+              </div>
+              <div className='relative h-[100rem]'>
+                <img src="/photography/nightlife/5.jpg" layout="fill" objectFit="contain"/>
+              </div>
+              <div className='relative h-[100rem]'>
+                <img src="/photography/nightlife/6.jpg" layout="fill" objectFit="contain"/>
+              </div>
+            </div>
+
+            <div className='mx-auto my-auto flex flex-col gap-1 w-full h-full mt-40 pt-2 px-2 mb-16 bg-black z-10'>
+              <div className='relative h-[100rem]'>
+                <img src="/photography/nightlife/7.jpg" layout="fill" objectFit="contain"/>
+              </div>
+              <div className='relative h-[100rem]'>
+                <img src="/photography/nightlife/8.jpg" layout="fill" objectFit="contain"/>
+              </div>
+              <div className='relative h-[100rem]'>
+                <img src="/photography/nightlife/9.jpg" layout="fill" objectFit="contain"/>
+              </div>
+            </div>
+          </>
+        )
+      case 1:
+        return (
+          <>
+            {/* <span className='absolute mt-20 ml-2 z-0'>light shines in the darkeness of the night;</span> */}
+
+            <div className='mx-auto my-auto flex flex-col gap-1 w-full h-full mt-40 pt-2 px-2 bg-black z-10'>
+              <div className='relative h-[100rem]'>
+                <img src="/photography/cinematic/1.jpg" layout="fill" objectFit="contain"/>
+              </div>
+              <div className='relative h-[100rem]'>
+                <img src="/photography/cinematic/2.jpg" layout="fill" objectFit="contain"/>
+              </div>
+              <div className='relative h-[100rem]'>
+                <img src="/photography/cinematic/3.jpg" layout="fill" objectFit="contain"/>
+              </div>
+            </div>
+
+            <div className='mx-auto my-auto flex flex-col gap-1 w-full h-full mt-40 pt-2 px-2 bg-black z-10'>
+              <div className='relative h-[100rem]'>
+                <img src="/photography/cinematic/4.jpg" layout="fill" objectFit="contain"/>
+              </div>
+              <div className='relative h-[100rem]'>
+                <img src="/photography/cinematic/5.jpg" layout="fill" objectFit="contain"/>
+              </div>
+              <div className='relative h-[100rem]'>
+                <img src="/photography/cinematic/6.jpg" layout="fill" objectFit="contain"/>
+              </div>
+            </div>
+
+            <div className='mx-auto my-auto flex flex-col gap-1 w-full h-full mt-40 pt-2 px-2 mb-16 bg-black z-10'>
+              <div className='relative h-[100rem]'>
+                <img src="/photography/cinematic/7.jpg" layout="fill" objectFit="contain"/>
+              </div>
+              <div className='relative h-[100rem]'>
+                <img src="/photography/cinematic/8.jpg" layout="fill" objectFit="contain"/>
+              </div>
+              <div className='relative h-[100rem]'>
+                <img src="/photography/cinematic/9.jpg" layout="fill" objectFit="contain"/>
+              </div>
+            </div>
+          </>
+        )
+      case 2:
+        return (
+          <>
+            {/* <span className='absolute mt-20 ml-2 z-0'>light shines in the darkeness of the night;</span> */}
+
+            <div className='mx-auto my-auto flex flex-col gap-1 w-full h-full mt-40 pt-2 px-2 bg-black z-10'>
+              <div className='relative h-[100rem]'>
+                <img src="/photography/portraits/1.jpg" layout="fill" objectFit="contain"/>
+              </div>
+              <div className='relative h-[100rem]'>
+                <img src="/photography/portraits/2.jpg" layout="fill" objectFit="contain"/>
+              </div>
+              <div className='relative h-[100rem]'>
+                <img src="/photography/portraits/3.jpg" layout="fill" objectFit="contain"/>
+              </div>
+            </div>
+
+            <div className='mx-auto my-auto flex flex-col gap-1 w-full h-full mt-40 pt-2 px-2 bg-black z-10'>
+              <div className='relative h-[100rem]'>
+                <img src="/photography/portraits/4.jpg" layout="fill" objectFit="contain"/>
+              </div>
+              <div className='relative h-[100rem]'>
+                <img src="/photography/portraits/5.jpg" layout="fill" objectFit="contain"/>
+              </div>
+              <div className='relative h-[100rem]'>
+                <img src="/photography/portraits/6.jpg" layout="fill" objectFit="contain"/>
+              </div>
+            </div>
+
+            <div className='mx-auto my-auto flex flex-col gap-1 w-full h-full mt-40 pt-2 px-2 mb-16 bg-black z-10'>
+              <div className='relative h-[100rem]'>
+                <img src="/photography/portraits/7.jpg" layout="fill" objectFit="contain"/>
+              </div>
+              <div className='relative h-[100rem]'>
+                <img src="/photography/portraits/13.jpg" layout="fill" objectFit="contain"/>
+              </div>
+              <div className='relative h-[100rem]'>
+                <img src="/photography/portraits/9.jpg" layout="fill" objectFit="contain"/>
+              </div>
+            </div>
+
+            <div className='mx-auto my-auto flex flex-col gap-1 w-full h-full mt-40 pt-2 px-2 overflow-scroll mb-16 bg-black z-10'>
+              <div className='relative h-[100rem]'>
+                <img src="/photography/portraits/10.jpg" layout="fill" objectFit="contain"/>
+              </div>
+              <div className='relative h-[100rem]'>
+                <img src="/photography/portraits/11.jpg" layout="fill" objectFit="contain"/>
+              </div>
+              <div className='relative h-[100rem]'>
+                <img src="/photography/portraits/12.jpg" layout="fill" objectFit="contain"/>
+              </div>
+            </div>
+          </>
+        )
+    }
+  }
+
+  return (
+    <div className='flex w-full h-screen overflow-scroll pr-2'>
+      {renderPhotos()}
+    </div>
+  )
+}
