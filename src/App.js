e86f1ce381d027814d8ca0458707ca6205cd7d72
Your name here <elia.orsini@hotmail.com>
2022-05-28 19:31:53 +0100

    website not ready message

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ import Index from './index.jsx'
 function App() {
   return (
     <div className="App">
-      <Index />
+      {/* <Index /> */}
+      <div className='flex h-screen bg-black'>
+        <div className='bg-black m-auto'>
+          <p className='text-white'>website not ready yet :c</p>
+          <p className='text-white'>working on it</p>
+        </div>
+      </div>
     </div>
   );
 }
