a57a7a430f76856ea625c421ca0296d81267e45a
Your name here <elia.orsini@hotmail.com>
2022-09-19 13:57:24 +0100

    fix calendly

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -4,7 +4,7 @@ export default function Calendar() {
     return (
       <div className="w-full h-screen">
         <div className="mx-auto my-auto h-80 mt-40">
-            <InlineWidget url="https://calendly.com/elia-orsini/30min?month=2022-09&date=2022-09-19" />
+            <InlineWidget url="https://calendly.com/elia-orsini/30min" />
         </div>
       </div>
     )
