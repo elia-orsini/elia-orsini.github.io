8080b165f1a3d491fd5e731ed861df293d0102b3
Your name here <elia.orsini@hotmail.com>
2022-11-12 08:36:28 +0000

    feat: change homepage

--- src/Code.jsx
+++ src/Code.jsx
@@ -1,31 +1,32 @@
 
 export default function Code() {
   return (
-    <div className="flex flex-col">
-      <div className='flex flex-col sm:flex-row sm:mt-44 mt-10'>
-        <div>
-          <p className="text-left font-black text-2xl sm:text-xl pb-4">
-            Side projects
-          </p>
-          <p className="text-left font-black text-2xl sm:text-xl pb-4 hidden sm:block mt-[21em]">
-            Websites I built
-          </p>
-        </div>
-        <ul className="text-left sm:ml-20">
+    <div className="font-ia flex flex-col w-full">
+      <div className='flex flex-col sm:mt-44 mt-10'>
+        <p className="text-left font-black text-2xl sm:text-xl pb-4">
+          Side projects
+        </p>
+        <ul className="text-left">
           <li className="py-2">
           <a href="https://github.com/elia-orsini/BlenderWebViewer" target="_blank" rel="noreferrer"><h1 className="font-black underline underline-offset-1 decoration-2">BlenderWebViewer</h1></a>
             <p className="text-sm text-gray-200">A web viewer for blender.</p>
-            <p className="text-sm font-light text-gray-400">PYTHON - REACT - REACT THREE FIBER</p>
+            <p className="text-xs font-light text-gray-400">PYTHON - REACT - REACT THREE FIBER</p>
           </li>
           <li className="py-2">
-          <a href="https://github.com/elia-orsini/mars-imagery-classification" target="_blank" rel="noreferrer"><h1 className="font-black underline underline-offset-1 decoration-2">mars-imagery-classification</h1></a>
-            <p className="text-sm  text-gray-200">Neural Network that classifies images taken by the Curiosity rover on the surface of Mars.</p>
-            <p className="text-xs font-light text-gray-400">PYTHON - TENSORFLOW</p>
+            <a href="https://github.com/elia-orsini/plantie" target="_blank" rel="noreferrer"><h1 className="font-black underline underline-offset-1 decoration-2 block inline">plantie</h1></a>
+            <a href="https://plantie.fly.dev" target="_blank" rel="noreferrer"><h1 className="block inline pl-6 text-sm font-base underline underline-offset-1 decoration-1">demo</h1></a>
+            <p className="text-sm text-gray-200">peer-to-peer plant sitting idea.</p>
+            <p className="text-xs font-light text-gray-400">REMIX - REACT - TAILWIND CSS</p>
           </li>
           <li className="py-2">
-          <a href="https://r124.vercel.app" target="_blank" rel="noreferrer"><h1 className="font-black underline underline-offset-1 decoration-2">HousesOfRome map</h1></a>
-            <p className="text-sm text-gray-200">3D interactive map optimised for the browser.</p>
+            <a href="https://r124.vercel.app" target="_blank" rel="noreferrer"><h1 className="font-black underline underline-offset-1 decoration-2">HoR Map</h1></a>
+            <p className="text-sm text-gray-200">3D interactive map.</p>
             <p className="text-xs font-light text-gray-400">JAVASCRIPT - THREE.JS</p>
+          </li>
+          <li className="py-2">
+            <a href="https://scenes-liard.vercel.app/" target="_blank" rel="noreferrer"><h1 className="font-black underline underline-offset-1 decoration-2 block inline">Ancient Rome Rooms</h1></a>
+            <p className="text-sm text-gray-200">3D rooms in Ancient Rome.</p>
+            <p className="text-xs font-light text-gray-400">REACT - REACT THREE FIBER - RAPIER</p>
           </li>
           <li className="py-2">
             <a href="https://github.com/sebastian-axell/munchpal" target="_blank" rel="noreferrer"><h1 className="font-black underline underline-offset-1 decoration-2 block inline">munchpal</h1></a>
@@ -34,36 +35,18 @@ export default function Code() {
             <p className="text-xs font-light text-gray-400">DJANGO - REACT - REDUX - TAILWIND CSS</p>
           </li>
           <li className="py-2">
-            <a href="https://github.com/elia-orsini/plantie" target="_blank" rel="noreferrer"><h1 className="font-black underline underline-offset-1 decoration-2 block inline">plantie</h1></a>
-            <a href="https://plantie.fly.dev" target="_blank" rel="noreferrer"><h1 className="block inline pl-6 text-sm font-base underline underline-offset-1 decoration-1">demo</h1></a>
-            <p className="text-sm text-gray-200">peer-to-peer plant-sitting.</p>
-            <p className="text-xs font-light text-gray-400">REMIX - REACT - TAILWIND CSS</p>
+            <a href="https://github.com/elia-orsini/mars-imagery-classification" target="_blank" rel="noreferrer"><h1 className="font-black underline underline-offset-1 decoration-2">Mars Neural Net</h1></a>
+            <p className="text-sm  text-gray-200">Neural Network that classifies images taken on Mars.</p>
+            <p className="text-xs font-light text-gray-400">PYTHON - TENSORFLOW</p>
           </li>
-          {/* <li className="py-2">
-          <a href="https://r124.vercel.app" target="_blank" rel="noreferrer"><h1 className="font-black underline underline-offset-1 decoration-2">3d Immersive Scenes</h1></a>
-            <p className="text-sm text-gray-200">3D interactive map optimised for the browser.</p>
-            <p className="text-xs font-light text-gray-400">REACT - REACT-THREE-FIBER - RAPIER</p>
-          </li> */}
-          <ul className="text-left pb-20 hidden sm:block mt-20">
-            <li className="py-2">
-              <a href="https://insomniaevents.xyz" target="_blank" rel="noreferrer"><h1 className="font-black underline underline-offset-1 decoration-2">Insomnia</h1></a>
-              <p className="text-sm text-gray-200">website for Insomnia.</p>
-              <p className="text-xs font-light text-gray-400">REACT - TAILWIND CSS</p>
-            </li>
-            <li className="py-2">
-              <a href="https://lamacinasonora.com" target="_blank" rel="noreferrer"><h1 className="font-black underline underline-offset-1 decoration-2">La Macina Sonora</h1></a>
-              <p className="text-sm text-gray-200">website for the recording studio La Macina Sonora.</p>
-              <p className="text-xs font-light text-gray-400">REACT - TAILWIND CSS</p>
-            </li>
-          </ul>
         </ul>
       </div>
 
-      <div className='flex flex-col sm:flex-row sm:mt-20 mt-20'>
-        <p className="text-left font-black text-2xl sm:text-xl pb-4 block sm:hidden">
+      <div className='flex flex-col mt-20'>
+        <p className="text-left font-black text-2xl sm:text-xl pb-4 block">
           Websites I built
         </p>
-        <ul className="text-left pb-20 block sm:hidden">
+        <ul className="text-left pb-20 block">
           <li className="py-2">
             <a href="https://insomniaevents.xyz" target="_blank" rel="noreferrer"><h1 className="font-black underline underline-offset-1 decoration-2">Insomnia</h1></a>
             <p className="text-sm text-gray-200">website for Insomnia.</p>
--- src/HomeContent.jsx
+++ src/HomeContent.jsx
@@ -1,27 +1,46 @@
 
 import React, { useRef, useEffect } from 'react'
 import Typewriter from 'typewriter-effect';
+import { BoxArrowUpRight } from 'react-bootstrap-icons';
+
+const LinkIcon = (props) => {
+
+  return (
+    <>
+      <span className='text-white'> {props.title}</span>
+    <a href={props.href} target="_blank">
+      <BoxArrowUpRight size={12} className='text-white ml-1 mb-1 inline-block'/>
+    </a>
+    </>
+)}
 
 export default function HomeContent({ ...props }) {
   return (
-    <>
-      <div className="absolute text-10 xl:text-[4.5rem] mr-[30rem] mt-[27rem] font-bold z-10 xl:opacity-100 opacity-0">
-        <Typewriter
-          onInit={(typewriter) => {
-            typewriter.typeString("Hiya, i'm Elia")
-            .start()
-          }}
-          options={{
-            autoStart: true,
-            deleteSpeed: 0.5,
-            delay: 140
-          }}
-          />
+    <div className='w-full mt-10 sm:mt-44 text-left columns-1 leading-10 tracking-tight'>
+      <div>
+        <p className='text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>hiya, I'm Elia</p>
+        <p className='text-lg sm:text-xl font-light sm:mt-2 text-gray-400'>but people just call me el</p>
+        <br />
+        <br />
+        <p className='text-xl sm:text-2xl font-bold'>Please check out my works!</p>
+        <p className='text-base font-light text-gray-400'>and send me an email if u like them ;)</p>
+        <br />
+        <br />
+        <p className='text-xl sm:text-2xl font-bold'>How do I fill my time?</p>
+        <p className='text-base font-light text-gray-400'>
+          I am currently a junior developer at <LinkIcon title="IMG Arena" href='https://imgarena.com'/> and working on projects
+          like <LinkIcon title="Archive.pdf" href='https://www.archivepdf.net/'/>, <LinkIcon title="IN$0MN1A" href='https://insomniaevents.xyz/'/> or
+          <LinkIcon title="plantie" href='https://plantie.fly.dev/'/>.
+          I sometimes render cool distopic 3D stuff, I read non-fiction books, drink coffee/craft beer/cheap wine or stick and poke my legs.
+          I run (but haven't done that in a bit) and journal my days and have a little digital garden.
+        </p>
       </div>
 
-      <video className='w-60 sm:w-80 mt-4 sm:mt-0' width="350" playsInline autoPlay muted loop >
-        <source src="/3drender.mp4" type="video/mp4"/>
-      </video>
-    </>
+      {/* <div>
+        <video className='hidden md:block w-60 mt-4 sm:mt-0' width="350" playsInline autoPlay muted loop >
+          <source src="/3drender.mp4" type="video/mp4"/>
+        </video>
+      </div> */}
+    </div>
   )
 }
