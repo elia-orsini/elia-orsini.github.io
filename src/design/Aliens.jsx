01a8f749e54551fe5cc617f155405c159035b795
Your name here <elia.orsini@hotmail.com>
2023-06-07 11:43:21 +0100

    feat: add extra alien image

--- src/design/Aliens.jsx
+++ src/design/Aliens.jsx
@@ -7,11 +7,14 @@ export default function Aliens() {
       {/* <span className='absolute mt-[8rem] ml-2 z-0 text-[1rem]'>4D4554414C</span> */}
 
       <div className='mx-auto columns-1 lg:columns-2 gap-2 w-full h-max mt-10 sm:mt-40 lg:mt-60 xl:mt-60 mb-0 sm:mb-40 xl:pr-4 z-10 bg-black'>
-        <div className='relative my-0'>
-          <Image src="/design/aliens/alien1.jpg" />
+        <div className='relative my-3'>
+          <Image src="/design/aliens/alien3.jpg" />
         </div>
         <div className='relative my-3'>
           <Image src="/design/aliens/alien2.jpg" />
+        </div>
+        <div className='relative my-0'>
+          <Image src="/design/aliens/alien1.jpg" />
         </div>
       </div>
     </>
